4f089330056722c92c9c69937f806963348dd9ca
u-dubnikova <u-dubnikova@ya.ru>
2020-11-07 21:27:15 +0300

    1.1.4
    hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.exception.SQLGrammarException;
 
 import java.util.List;
 
@@ -12,31 +18,94 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Configuration cfg = Util.getConfiguration();
 
+        Session session = Util.getSession(cfg);
+
+        Transaction tx = session.beginTransaction();
+        Query query = session.createSQLQuery("create table User (id INT auto_increment, name VARCHAR(45), lastName VARCHAR(45), age INT, constraint user_pk primary key (id));");
+        try{
+        query.executeUpdate();
+        } catch (Exception e){ }
+        tx.commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
+        Configuration cfg = Util.getConfiguration();
+        cfg.addAnnotatedClass(User.class);
+        Session session = Util.getSession(cfg);
+
+        Transaction tx = session.beginTransaction();
+        Query query = session.createSQLQuery("drop table User ");
+        try {
+            query.executeUpdate();
+        } catch (Exception e){ }
+
+        tx.commit();
+        session.close();
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Configuration cfg = Util.getConfiguration();
+        cfg.addAnnotatedClass(User.class);
+        Session session = Util.getSession(cfg);
 
+        Transaction tx = session.beginTransaction();
+
+        User user = new User(name, lastName, age);
+        session.save(user);
+
+        /*Query query = session.createSQLQuery("insert into user (name, lastName, age) values (:name, :lastName , :age) ");
+        query.setParameter("name", name);
+        query.setParameter("lastName", lastName);
+        query.setParameter("age", age);
+        query.executeUpdate();*/
+        tx.commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Configuration cfg = Util.getConfiguration();
+        cfg.addAnnotatedClass(User.class);
+        Session session = Util.getSession(cfg);
+        Transaction tx = session.beginTransaction();
+        try {
+            session.delete(session.get(User.class,id));
+        } catch (Exception e){ }
+        tx.commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Configuration cfg = Util.getConfiguration();
+        cfg.addAnnotatedClass(User.class);
+        Session session = Util.getSession(cfg);
+
+        Query query  = session.createQuery("from User");
+        List list = query.list();
+
+        session.close();
+
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
+        Configuration cfg = Util.getConfiguration();
+        cfg.addAnnotatedClass(User.class);
+        Session session = Util.getSession(cfg);
+        Transaction tx = session.beginTransaction();
 
+        Query query = session.createQuery("delete from User");
+        query.executeUpdate();
+
+        tx.commit();
+        session.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -16,5 +22,25 @@ public class Util {
 
     public static void closeConnection(Connection con) throws SQLException {
         con.close();
+    }
+
+    public static Configuration getConfiguration() {
+        Properties properties = new Properties();
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.setProperty(Environment.USER, "user");
+        properties.setProperty(Environment.PASS, "user");
+        properties.setProperty(Environment.URL, "jdbc:mysql://127.0.0.1:3306/java_users?serverTimezone=Europe/Moscow");
+        properties.setProperty("hibernate.id.new_generator_mappings", "false");
+
+        Configuration cfg = new Configuration();
+        cfg.setProperties(properties);
+
+        return cfg;
+    }
+
+    public static Session getSession(Configuration cfg) {
+        SessionFactory factory = cfg.buildSessionFactory();
+        return factory.openSession();
     }
 }
